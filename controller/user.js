caa2178adf9854713011bde2bb65fdf517cfe293
Adri Candra Saputra Mangidi <adricandrasaputramangidi@gmail.com>
2023-05-25 03:20:33 +0800

    updetan yang sama dengan sebelumnya

--- controller/user.js
+++ controller/user.js
@@ -4,21 +4,26 @@ const tanggal = moment().format('DD MMMM YYYY, h:mm:ss a');
 module.exports = class {
     static async add(req, res) {
         const { id, fullName, givenName, familyName, imageUrl, email } = req.body;
-        await user
-            .insertMany({
-                id_google: id,
-                Nama_lengkap: fullName,
-                nama_depan: givenName,
-                nama_belakang: familyName,
-                email: email,
-                img_url: imageUrl,
-                createdAt: tanggal,
-            })
-            .then(() => {
-                res.status(201).json({ message: 'User berhasil ditambahkan' });
-            })
-            .catch((err) => {
-                res.status(400).json({ message: err.message });
-            });
+        const cek = await user.findOne({ id_google: id });
+        if (!cek) {
+            await user
+                .insertMany({
+                    id_google: id,
+                    Nama_lengkap: fullName,
+                    nama_depan: givenName,
+                    nama_belakang: familyName,
+                    email: email,
+                    img_url: imageUrl,
+                    createdAt: tanggal,
+                })
+                .then(() => {
+                    res.status(201).json({ message: 'User berhasil ditambahkan' });
+                })
+                .catch((err) => {
+                    res.status(400).json({ message: err.message });
+                });
+        } else {
+            res.status(400).json({ message: 'Akun sudah terdaftar' });
+        }
     }
 };
